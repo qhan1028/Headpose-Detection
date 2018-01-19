3445ed0f559f88e37dd7ddcaac42a8257bdad308
qhan <b03902089@ntu.edu.tw>
2018-01-19 16:24:51 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
-mkdir -p model
-mv shape_predictor_68_face_landmarks.dat model
+bzip2 -vd shape_predictor_68_face_landmarks.dat.bz2
+mkdir -vp model
+mv -v shape_predictor_68_face_landmarks.dat model
